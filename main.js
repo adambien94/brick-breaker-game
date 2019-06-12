cd5adb6a21a454c8381c0f7267137ee5de3ae382
Adam Bien <adam.bien94@gmail.com>
2019-06-12 19:41:03 +0200

    smaller bricks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const W = 600;
 const H = 500;
 const maxBricks = 10;
 const brickWidth = W / maxBricks;
-const brickHeight = 35;
+const brickHeight = 30;
 var gameStarted = false;
 var bounceCount = 0;
 var ammo = document.getElementById("ammo");
@@ -558,13 +558,13 @@ function Brick(x, y, type) {
       strokeWeight(2);
       triangle(
         this.x - 5 + brickWidth / 2,
-        this.y + 30,
+        this.y + 27,
         this.x + 5 + brickWidth / 2,
-        this.y + 30,
+        this.y + 27,
         this.x + brickWidth / 2,
-        this.y + 18
+        this.y + 15
       );
-      ellipse(this.x + brickWidth / 2, this.y + 15, 10, 20);
+      ellipse(this.x + brickWidth / 2, this.y + 13, 10, 20);
       fill(77, 112, 168, 255 * (this.life / 100));
     } else {
       fill(77, 112, 168, 255 * (this.life / 100));
